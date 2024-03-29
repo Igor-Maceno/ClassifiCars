0c43955de6b4c2cb0d60258661e04e9ef91f55fd
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-03-29 19:55:02 -0300

    Navbar and footer
    Inicio do desenvolvimento do menu de navegação e do rodapé.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Classificar</h1>
+      <Footer/>
     </div>
   );
 }
