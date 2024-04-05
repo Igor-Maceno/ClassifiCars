56de25174c5c915188568b63cba5bf1a0ff38f35
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-04-05 01:21:46 -0300

    vehicle

---
+++ frontend/src/pages/vehicle/Vehicle.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+    name: 'Hyundai Tucson',
+    
+}
+
+const Vehicle = (props: Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Vehicle
