b968f1eacab431173916b3c14620d8e82d0bedb5
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-04-04 23:10:11 -0300

    card

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -11,10 +11,14 @@ type Props = {
 
 const Card = (props: Props) => {
   return (
-    <div className="card_container">
+    <div className={styles.card_container}>
+      <img src={props.img} alt=""/>
       <h2>{props.name}</h2>
       <p>{props.description}</p>
-      
+      <hr />
+      <h3>R${props.price}</h3>
+      <p>{props.year}</p>
+      <button>Saiba mais</button>
     </div>
   );
 };
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import styles from './Home.module.css';
-import Card from '../../components/card/Card';
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import styles from "./Home.module.css";
+import Card from "../../components/card/Card";
+import image from "../../img/image.png";
 
 const Home = () => {
+  const car = [
+    {
+      img: image,
+      name: "Hyundai Tucson",
+      description: "2.7 MPFI GLS 24V 180CV 4WD GASOLINA 4P MANUAL",
+      price: "97.500",
+      year: "2024",
+    },
+    {
+      img: image,
+      name: "Hyundai HB20",
+      description: "2.7 MPFI GLS 24V 180CV 4WD GASOLINA 4P MANUAL",
+      price: "49.500",
+      year: "2024",
+    },
+    {
+      img: image,
+      name: "Hyundai Azera",
+      description: "2.7 MPFI GLS 24V 180CV 4WD GASOLINA 4P MANUAL",
+      price: "59.500",
+      year: "2024",
+    },
+  ];
   return (
     <div>
       <div className={styles.search}>
-        <SearchIcon/>
-        <input type="text" name='search' placeholder='Busque por marca ou modelo'/>
+        <SearchIcon />
+        <input
+          type="text"
+          name="search"
+          placeholder="Busque por marca ou modelo"
+        />
         <label htmlFor="search">Pesquisar</label>
       </div>
-      <div className={styles.news}>
-        <Card name="Igor"/>
+      <div className={styles.news_section}>
+        <h1>Destaques</h1>
+        <div className={styles.news}>
+          {car.map(car=>(
+            <Card
+            img={car.img}
+            name={car.name}
+            description={car.description}
+            price={car.price}
+            year={car.year}
+          />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
