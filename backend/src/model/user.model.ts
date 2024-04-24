99b816fcfad3bca6190a6ecfbd41b43e7461a9c9
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-04-24 13:08:15 -0300

    change model

--- backend/src/model/user.model.ts
+++ backend/src/model/user.model.ts
@@ -1,41 +1,8 @@
 
-class User {
-    private name: string;
-    private email: string;
-    private senha: string;
-  
-    constructor(name: string, email: string, senha: string) {
-      this.name = name;
-      this.email = email;
-      this.senha = senha;
-    }
-  
-    // Getters
-    public getNome(): string {
-      return this.name;
-    }
-  
-    public getEmail(): string {
-      return this.email;
-    }
-  
-    public getSenha(): string {
-      return this.senha;
-    }
-  
-    // Setters
-    public setNome(name: string): void {
-      this.name = name;
-    }
-  
-    public setEmail(email: string): void {
-      this.email = email;
-    }
-  
-    public setSenha(senha: string): void {
-      this.senha = senha;
-    }
-  }
-  
-  export default User;
-  
+export default class User {
+  constructor(
+    name: string,
+    email: string,
+    senha: string
+  ) { }
+}
