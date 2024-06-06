ad0286f8bffa1987a66889082a0aa7213f252c2a
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-06-06 18:50:55 -0300

    footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -5,8 +5,34 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <nav>
+        <ul>
+          <th>Comprar</th>
+          <li>Usados</li>
+          <li>Seminovos</li>
+          <li>Novos</li>
+        </ul>
+        <ul>
+          <th>Vender</th>
+          <li>Carro</li>
+          <li>Moto</li>
+          <li>Anúncios</li>
+        </ul>
+        <ul>
+          <th>Notícias</th>
+          <li>Carro</li>
+          <li>Moto</li>
+          <li>Recentes</li>
+        </ul>
+        <ul>
+          <th>Acesso</th>
+          <li>Login</li>
+          <li>Cadastro</li>
+          <li>Recuperar acesso</li>
+        </ul>
+      </nav>
       <NavLink to="/" className={styles.brand}>
-        Classifi<span>Cars</span>
+        Classifi<span>Cars</span> &copy; 2024
       </NavLink>
     </footer>
   );
